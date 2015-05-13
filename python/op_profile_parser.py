c753bfb41fb64464d5af911ddb0df18efb113bc6
DerrickStuckey <derrickstuckey@gmail.com>
2015-05-13 01:08:26 -0400

    pulls minimal id data from profiles

--- python/op_profile_parser.py
+++ python/op_profile_parser.py
@@ -30,6 +30,8 @@ def extract_provider(record):
 opn_gen_zipfilename = "../../12192014_RFRSHDTL.zip"
 profile_filename = "OPPR_SPLMTL_PH_PRFL_12192014.csv"
 
+output_filename = "op_profile_min.csv"
+
 with open(opn_gen_zipfilename) as thezip:
     unzipped = zipfile.ZipFile(thezip)
     # names = unzipped.namelist()
@@ -47,7 +49,24 @@ with open(opn_gen_zipfilename) as thezip:
         first_provider = extract_provider(first)
         print first_provider.first_name, " ", first_provider.middle_initial, " ", first_provider.last_name
 
-        for i in range(0,5,1):
-            record = reader.next()
-            this_provider = extract_provider(record)
-            print this_provider.first_name, " ", this_provider.middle_initial, " ", this_provider.last_name
+        # for i in range(0,5,1):
+        #     record = reader.next()
+        #     this_provider = extract_provider(record)
+        #     print this_provider.first_name, " ", this_provider.middle_initial, " ", this_provider.last_name
+
+        # truncate output filename
+        # with open(output_filename, 'w') as output_file:
+        #     pass
+
+        with open(output_filename, 'w') as output_file:
+            csvwriter = csv.writer(output_file)
+            csvwriter.writerow(['FirstName','MiddleInitial','LastName','ZipCode5','NPI','OpProfileID'])
+            while(True):
+                thisrecord = reader.next()
+                if (thisrecord == None):
+                    break
+                cur_provider = extract_provider(thisrecord)
+                csvwriter.writerow([cur_provider.first_name, cur_provider.middle_initial, cur_provider.last_name,
+                                    cur_provider.zip_first_5, cur_provider.npi, cur_provider.op_profile_id])
+
+
